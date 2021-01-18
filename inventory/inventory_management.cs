f658a881bce9151fdc6cfd5b3ee6a07c6b08d941
Tomas Rigaux <tomas@rigaux.com>
2021-01-18 20:20:16 +0100

    Added Skeleton Kit auto refill

--- inventory/inventory_management.cs
+++ inventory/inventory_management.cs
@@ -1,8 +1,12 @@
 
+const String skeletonKitSuffix = "Skeleton Kit";
+
 IMyTerminalBlock raws;
 IMyTerminalBlock ingots;
 IMyTerminalBlock components;
 List<IMyTerminalBlock> blocks;
+
+Dictionary<MyItemType, MyFixedPoint> skeletonItems = new Dictionary<MyItemType, MyFixedPoint>();
 
 public Program() {
     Runtime.UpdateFrequency = UpdateFrequency.Update100;
@@ -16,6 +20,8 @@ public void init() {
     
     blocks = new List<IMyTerminalBlock>();
     GridTerminalSystem.GetBlocks(blocks);
+    skeletonItems.Add(new MyItemType("MyObjectBuilder_Component", "SteelPlate"), 50);
+    skeletonItems.Add(new MyItemType("MyObjectBuilder_Component", "InteriorPlate"), 50);
 }
 
 public Boolean isOre(MyInventoryItem item) {
@@ -48,13 +54,14 @@ List<MyItemType> ingotsBlacklist = new List<MyItemType> {
 };
 
 public void Main(string argument, UpdateType updateSource) {
+    MyFixedPoint amount;
     foreach(IMyTerminalBlock blk in blocks) {
         int inventories = blk.InventoryCount;
         List<MyInventoryItem> items = new List<MyInventoryItem>();
-        
+
         for(int i = 0; i < inventories; i++) {
             IMyInventory inventory = blk.GetInventory(i);
-            
+
             if(blk.EntityId != raws.EntityId && !oreBlacklist.Contains(blockType(blk))) {
                 inventory.GetItems(items, isOre);
                 foreach(MyInventoryItem item in items) {
@@ -73,7 +80,36 @@ public void Main(string argument, UpdateType updateSource) {
                     && blk.CubeGrid.EntityId == components.CubeGrid.EntityId) {
                 inventory.GetItems(items, isComponent);
                 foreach(MyInventoryItem item in items) {
-                    inventory.TransferItemTo(components.GetInventory(), item, null);
+                    if(blk.CustomName.EndsWith(skeletonKitSuffix) &&
+                            skeletonItems.TryGetValue(item.Type, out amount)) {
+                        if(amount > inventory.GetItemAmount(item.Type)) {
+                            amount -= inventory.GetItemAmount(item.Type);
+                            MyInventoryItem? itemStorage = components.GetInventory().FindItem(item.Type);
+                            if(itemStorage.HasValue) {
+                                inventory.TransferItemFrom(components.GetInventory(), itemStorage.Value, amount);
+                            }
+                        } else {
+                            amount = inventory.GetItemAmount(item.Type) - amount;
+                            inventory.TransferItemTo(components.GetInventory(), item, amount);
+                        }
+                    } else {
+                        inventory.TransferItemTo(components.GetInventory(), item, null);
+                    }
+                }
+            }
+        }
+
+        if(blk.CustomName.EndsWith(skeletonKitSuffix)) {
+            for(int i = 0; i < inventories; i++) {
+                IMyInventory inventory = blk.GetInventory(i);
+                foreach(var item in skeletonItems) {
+                    if(item.Value > inventory.GetItemAmount(item.Key)) {
+                        amount = item.Value - inventory.GetItemAmount(item.Key);
+                        MyInventoryItem? itemStorage = components.GetInventory().FindItem(item.Key);
+                        if(itemStorage.HasValue) {
+                            inventory.TransferItemFrom(components.GetInventory(), itemStorage.Value, amount);
+                        }
+                    }
                 }
             }
         }
